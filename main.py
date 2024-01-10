fdb047b794a023555358e8736c9eb44a12e6469b
chernozubova <chenolika@mail.ru>
2024-01-10 16:51:22 +0700

    create menu

--- main.py
+++ main.py
@@ -114,3 +114,36 @@ class Work:
                 print("Ошибка: Возраст должен быть числом. Попробуйте снова.")
 
         return Worker(name, surname, age)
+
+    def menu():
+        work = None
+        boss = None
+
+        while True:
+            print("Главное меню:")
+            print("1. Создать фирму.")
+            print("2. Создать директора(количество - 1).")
+            print("3. Создать сотрудника.(количество - не ограничено)")
+            print("4. Вывести информацию о сотруднике.")
+            print("5. Вывести информацию о директоре.")
+            print("6. Вывести информацию о фирме.")
+            print("7. Выход из программы ")
+
+            choose = input("Выберите пункт меню: ")
+
+            if choose == "1":
+                pass
+            elif choose == "2":
+                pass
+            elif choose == "3":
+                pass
+            elif choose == "4":
+                pass
+            elif choose == "5":
+                pass
+            elif choose == "6":
+                pass
+            elif choose == "7":
+                pass
+            else:
+                pass
